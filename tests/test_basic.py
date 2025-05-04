0e9412b62e0a04422906290e41c003f755f346ab
Petros Koutsolampros <commits@orange-vertex.net>
2025-05-04 05:16:17 +0300

    Make the test data file path relative to the test file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,13 +3,14 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 from __future__ import annotations
+import os
 
 import erodius as m
 
 def test_version():
     assert m.__version__ == "0.0.1"
 
-
 def test_readMetaGraph():
-    mgd = m.readMetaGraph("./tests/data/barnsburySmall.graph", False)
+    graphFile = os.path.join(os.path.dirname(__file__), 'data', 'barnsburySmall.graph')
+    mgd = m.readMetaGraph(graphFile, False)
     assert mgd['shapeMaps'][0]['map'].get_name() == "/tmp/Rtmp3CVtFg/file408014dde327b.tsv"
